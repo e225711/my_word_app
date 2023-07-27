b0a59962f5a0440e2fee0a3462684b1e2b2dc16f
yasudakouki <e225703@ie.u-ryukyu.ac.jp>
2023-07-27 18:24:53 +0900

    編集機能追加

--- ryou_app.py
+++ ryou_app.py
@@ -39,6 +39,14 @@ class Model:
         self.cursor.execute('''INSERT INTO words (genre_id, word, details) VALUES (?, ?, ?)''', (genre_id, word, details))
         self.connection.commit()
 
+    def edit_word(self, id:int, word: str, details: str):
+        self.cursor.execute('''
+        UPDATE words
+        SET word=?,details=?
+        WHERE id=?
+        ''', (word, details,id))
+        self.connection.commit()
+
     def get_words(self, genre_id: int):
         self.cursor.execute('''SELECT * FROM words WHERE genre_id = ?''', (genre_id,))
         return self.cursor.fetchall()
@@ -148,7 +156,6 @@ class WordListFrame(tk.Frame):
         self.understand_check_button.place(relx=0, rely=1, anchor='sw')
         self.update()
 
-        
         for word in model.get_words(genre[0]):
             tk.Button(self, text=word[2],command=lambda g = genre, w=word: self.on_word_button_click(g, w)).pack(expand=True)
 
@@ -242,7 +249,7 @@ class WordDetailFrame(tk.Frame):
 
     def on_edit_button_click(self):
         print("編集button clicked!")
-        switcher.switchTo(AddWordFrame, self.genre)
+        switcher.switchTo(WordEditFrame, self.genre,self.word)
 
     def on_next_button_click(self,genre:list,word:list):
         print("次へbutton clicked!")
@@ -251,17 +258,8 @@ class WordDetailFrame(tk.Frame):
         while(search_word_list[i][0]!=word[0]):
             i+=1
 
-        #print(search_word_list[i+1][2])
 
         switcher.switchTo(WordDetailFrame, genre, search_word_list[i+1])
-
-
-        
-        
-
-        
-
-
 
     def on_before_button_click(self,genre:list,word:list):
         print("次へbutton clicked!")
@@ -273,6 +271,51 @@ class WordDetailFrame(tk.Frame):
         #print(search_word_list[i+1][2])
 
         switcher.switchTo(WordDetailFrame, genre, search_word_list[i-1])
+
+class WordEditFrame(tk.Frame):
+    def __init__(self, switcher: FrameSwitcher, model: Model, genre: list, word: list):  # 追加: model: Model
+        super().__init__(switcher.parent)
+        self.model= model
+        self.genre = genre
+        self.word = word
+
+    
+
+        self.center_frame = tk.Frame(self)
+        self.center_frame.place(relx=0.5, rely=0.5, anchor='center')
+
+        tk.Label(self.center_frame, text="単語名").grid(row=0, column=0, columnspan=2, pady=5)
+        self.word_name_entry = tk.Entry(self.center_frame)
+        self.word_name_entry.insert(tk.END,word[2])
+        self.word_name_entry.grid(row=1, column=0, columnspan=2, pady=5)
+
+        tk.Label(self.center_frame, text="単語の詳細").grid(row=2, column=0, columnspan=2, pady=5)
+        self.word_detail_entry = tk.Entry(self.center_frame)
+        self.word_detail_entry.insert(tk.END,word[3])
+        self.word_detail_entry.grid(row=3, column=0, columnspan=2, pady=5)
+
+        self.add_button = tk.Button(self.center_frame, text="完了", command=self.on_edit_button_click)
+        self.add_button.grid(row=4, column=1, pady=5)
+
+        self.cancel_button = tk.Button(self.center_frame, text="キャンセル", command=self.on_cancel_button_click)
+        self.cancel_button.grid(row=4, column=0, pady=5)
+
+        self.update()
+
+        
+
+    def on_cancel_button_click(self):
+        print("キャンセルbutton clicked!")
+        switcher.switchTo(WordListFrame, self.genre)
+
+    def on_edit_button_click(self):
+        print("edit完了button clicked!")
+        word_name = self.word_name_entry.get()
+        word_detail = self.word_detail_entry.get()
+        self.model.edit_word(self.word[0], word_name, word_detail)
+        switcher.switchTo(WordListFrame, self.genre)
+        # データベースに関する処理
+
 
 
 
